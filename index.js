85e4502689cfdacd6c6f8566068ef89262a06868
jigar-m-simformsolutions <jigar.m@simformsolutions.com>
2021-01-13 22:29:28 +0530

    first commit

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-let Bio = {};
-var i = 0;
+// let Bio = {};
+// var i = 0;
 
-// function showTable(){
-//     var xhttp = new XMLHttpRequest();
+// // function showTable(){
+// //     var xhttp = new XMLHttpRequest();
 
-//     xhttp.onreadystatechange = function(){[i]   
-//         if(this.readyState == 4 && this.status == 200){
-//             var response = JSON.parse(xhttp.responseText);
-//             var persons = response.Person;
+// //     xhttp.onreadystatechange = function(){[i]   
+// //         if(this.readyState == 4 && this.status == 200){
+// //             var response = JSON.parse(xhttp.responseText);
+// //             var persons = response.Person;
 
-//             var temp = "";
-//             // var div = document.createElement('tableDiv');
-//             // var table = document.createElement('table');
-//             // var thead = document.createElement('thead');
-//             // var tbody = document.createElement('tbody');
+// //             var temp = "";
+// //             // var div = document.createElement('tableDiv');
+// //             // var table = document.createElement('table');
+// //             // var thead = document.createElement('thead');
+// //             // var tbody = document.createElement('tbody');
 
-//             for(var i=0; i< persons.length; i++){
-//                 temp += '<tr>';
-//                 temp += '<td>'+persons[i].firstName+'</td>';
-//                 temp += '<td>'+persons[i].lastName+'</td>';
-//                 temp += '<td>'+persons[i].age+'</td>';
-//                 temp += '<td>'+persons[i].country+'</td></tr>';
-//             }
-//             document.getElementById('data').innerHTML = temp;
-//             // tbody.appendChild(temp);
-//             // table.appendChild(thead);
-//             // table.appendChild(tbody);
-//             // div.appendChild(table);
-//         }
-//     };
+// //             for(var i=0; i< persons.length; i++){
+// //                 temp += '<tr>';
+// //                 temp += '<td>'+persons[i].firstName+'</td>';
+// //                 temp += '<td>'+persons[i].lastName+'</td>';
+// //                 temp += '<td>'+persons[i].age+'</td>';
+// //                 temp += '<td>'+persons[i].country+'</td></tr>';
+// //             }
+// //             document.getElementById('data').innerHTML = temp;
+// //             // tbody.appendChild(temp);
+// //             // table.appendChild(thead);
+// //             // table.appendChild(tbody);
+// //             // div.appendChild(table);
+// //         }
+// //     };
 
-//     xhttp.open('GET','index.json',true);
-//     xhttp.send();
-// }
-// var xhttp = new XMLHttpRequest();
+// //     xhttp.open('GET','index.json',true);
+// //     xhttp.send();
+// // }
+// // var xhttp = new XMLHttpRequest();
 
-// xhttp.onreadystatechange = function(){[i]   
-//     if(this.readyState == 4 && this.status == 200){
-//         var response = JSON.parse(xhttp.responseText);
-//         var persons = response.Person;
+// // xhttp.onreadystatechange = function(){[i]   
+// //     if(this.readyState == 4 && this.status == 200){
+// //         var response = JSON.parse(xhttp.responseText);
+// //         var persons = response.Person;
 
-//         var temp = "";
+// //         var temp = "";
 
-//         for(var i=0; i< persons.length; i++){
-//             temp += '<tr>';
-//             temp += '<td>'+persons[i].firstName+'</td>';
-//             temp += '<td>'+persons[i].lastName+'</td>';
-//             temp += '<td>'+persons[i].age+'</td>';
-//             temp += '<td>'+persons[i].country+'</td></tr>';
-//         }
-//         document.getElementById('data').innerHTML = temp;
-//     }
-// };
+// //         for(var i=0; i< persons.length; i++){
+// //             temp += '<tr>';
+// //             temp += '<td>'+persons[i].firstName+'</td>';
+// //             temp += '<td>'+persons[i].lastName+'</td>';
+// //             temp += '<td>'+persons[i].age+'</td>';
+// //             temp += '<td>'+persons[i].country+'</td></tr>';
+// //         }
+// //         document.getElementById('data').innerHTML = temp;
+// //     }
+// // };
 
-// xhttp.open('GET','index.json',true);
-// xhttp.send();
+// // xhttp.open('GET','index.json',true);
+// // xhttp.send();
 
-// function f1(){
-//     var a = document.getElementsByTagName('button');
-//     a.addEventListener('click',showTable,true);
-// }
+// // function f1(){
+// //     var a = document.getElementsByTagName('button');
+// //     a.addEventListener('click',showTable,true);
+// // }
 
 const addData =(ev) =>{
     ev.preventDefault();
@@ -73,6 +73,7 @@ const addData =(ev) =>{
 
     i++;
     const Bio = Object.assign(Data);
+    Bio.push(Data);
     console.log(Bio);
     document.forms[0].reset();
     console.log(Bio.length);
@@ -86,7 +87,7 @@ const addData =(ev) =>{
         temp += '<td>'+Bio[j].age+'</td>';
         temp += '<td>'+Bio[j].country+'</td></tr>';
     }
-    console.log(temp);
+    // console.log(temp);
     document.getElementById('data').innerHTML = temp;
 
     let demo = document.querySelector('#demo');
@@ -102,3 +103,51 @@ const addData =(ev) =>{
 document.addEventListener('DOMContentLoaded', () =>{
     document.getElementById('btn').addEventListener('click',addData);   
 });
+
+// var row =0;
+// document.addEventListener('DOMContentLoaded', () =>{
+//     document.getElementById('btn').addEventListener('click',showTable);   
+// });
+
+// function showTable(){
+//     var fname =document.getElementById('fname').value;
+//     var lname =document.getElementById('lname').value;
+//     var age =document.getElementById('age').value;
+//     var country =document.getElementById('country').value;
+
+//     if(!fname || !lname || !age || !country){
+//         alert("Please fill all the details....");
+//         return ;
+//     }
+
+//     console.log(fname);
+
+//     var temp ="";
+//     for(var j=0; j< row; j++){
+//         temp += '<tr>';
+//         temp += '<td>'+fname+'</td>';
+//         temp += '<td>'+lname+'</td>';
+//         temp += '<td>'+age+'</td>';
+//         temp += '<td>'+country+'</td></tr>';
+//     }
+
+//     document.getElementById('data').innerHTML = temp;
+
+//     // var display = document.querySelector('#data');
+//     // var newRow = display.insertRow(row);
+
+//     // console.log(newRow);
+//     // var cell1 = newRow.insertCell(0);
+//     // var cell2 = newRow.insertCell(1);
+//     // var cell3 = newRow.insertCell(2);
+//     // var cell4 = newRow.insertCell(3);
+
+//     // console.log(cell1);
+
+//     // cell1.innerHTML = fname;
+//     // cell2.innerHTML = lname;
+//     // cell3.innerHTML = age;
+//     // cell4.innerHTML = country;
+
+//     row++;
+// }
